ad5eb83481c9fe475ff5394643cb6bc94ff0de03
Adrian <adrian.zafrarobledo@gmail.com>
2025-05-08 15:29:25 +0200

    Change to wofi

---
+++ modules/wofi.nix
@@ -0,0 +1,10 @@
+{
+  programs.wofi = {
+    enable = true;
+    settings = {
+      height = "200px"; # Set your desired height (e.g., 200px, 30%, etc.)
+      width = "300px"; # Optional: adjust width if needed
+      location = "center"; # Optional: position the window
+    };
+  };
+}
